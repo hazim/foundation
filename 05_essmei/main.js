f250ae5b882e4a4a82d53c74ee18b726a90ba44a
Hazim Sami (he/him) <1193645+hazim@users.noreply.github.com>
2024-10-27 19:55:03 +0100

    add the jquery file

---
+++ 05_essmei/main.js
@@ -0,0 +1,14 @@
+console.log('Hi there!');
+let name = 'Hazim';
+
+$('.show-filters').on('click', function () {
+
+    // console.log('you have clicked the filter');
+
+    $('.filters-list').toggle()
+    // if ($('.filters-list') === true) {
+    //     $('.filters-list').hide();
+    // } else {
+    //     $('.filters-list').show();
+    // }
+})
