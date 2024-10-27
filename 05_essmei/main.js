68be30e3b6d03a839122338b17b372f19af2fcce
Hazim Sami (he/him) <1193645+hazim@users.noreply.github.com>
2024-10-27 22:17:49 +0100

    project done

--- 05_essmei/main.js
+++ 05_essmei/main.js
@@ -1,14 +1,33 @@
-console.log('Hi there!');
-let name = 'Hazim';
-
 $('.show-filters').on('click', function () {
 
-    // console.log('you have clicked the filter');
+    // $('.filters-list').toggle(200)
 
-    $('.filters-list').toggle()
-    // if ($('.filters-list') === true) {
-    //     $('.filters-list').hide();
-    // } else {
-    //     $('.filters-list').show();
-    // }
+    if ($('.filters-list').is(':visible')) {
+        $('.filters-list').slideUp();
+        $('.show-filters').text('Show filters');
+    } else {
+        $('.filters-list').slideDown();
+        $('.show-filters').text('Hide filters');
+    }
+
+    return false;
+
+})
+
+
+$('.filters-list a').on('click', function() {
+
+    // console.log($(this).data('filter'));
+    let filter = $(this).attr('data-filter');
+
+
+    $('.product').hide();
+    $(filter).show();
+
+
+    $('.filters-list a').removeClass('selected');
+    $(this).addClass('selected');
+
+    return false;
+
 })
